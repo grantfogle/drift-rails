7a7c60de26b4f51b0742852a25c12cbddc76aeee
grantfogle <grantjfogle@gmail.com>
2024-01-09 20:38:22 -0700

    adding create route

--- app/controllers/flows_controller.rb
+++ app/controllers/flows_controller.rb
@@ -6,4 +6,18 @@ class FlowsController < ApplicationController
   def show
     @flow = Flow.find(params[:id])
   end
+
+  def new
+    @flow = Flow.new
+  end
+
+  def create
+    @flow = Flow.new(name: params[:flow][:name], location: params[:flow][:location], flow: params[:flow][:flow])
+
+    if @flow.save
+      redirect_to @flow
+    else
+      render :new
+    end
+  end
 end
