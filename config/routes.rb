5aa54906fd12dd1afb43a5ca4aa57ea564e0dff4
grantfogle <grantjfogle@gmail.com>
2024-01-26 18:18:43 -0700

    added basic signup form with error handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "flows#index"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
   
   resources :flows do
     resources :reports
