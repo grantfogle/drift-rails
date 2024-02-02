68f7f7bcb6558c6c70fd82cad737b6aafd71023c
grantfogle <grantjfogle@gmail.com>
2024-02-01 18:53:48 -0700

    finished login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(email: params[:email])
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: "Log in successful!"
+        else
+            flash[:alert] = "Invalid email or password";
+            render :index
+        end
     end
 
     def destroy
