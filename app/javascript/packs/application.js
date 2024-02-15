95fbef0e18863ec7e5f499a688bb513693131490
grantfogle <grantjfogle@gmail.com>
2024-02-14 18:05:22 -0700

    adding tests to ensure actioncable is working, trying to debug websockets now

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,24 +1,26 @@
+// // This file is automatically compiled by Webpack, along with any other files
+// // present in this directory. You're encouraged to place your actual application logic in
+// // a relevant structure within app/javascript and only use these pack files to reference
+// // that code so it'll be compiled.
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import {Application} from "stimulus"
-import StimulusReflex from "stimulus_reflex"
-import CableReady from "cable_ready"
-import consumer from "../channels/consumer"
-import "channels"
+window.Rails = require("@rails/ujs")
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
 import "controllers"
 
-const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
-application.load(context)
-
-StimulusReflex.initialize(application, { consumer, debug: true, CableReady })
-
 Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
--- app/reflexes/flow_search_reflex.rb
+++ app/reflexes/flow_search_reflex.rb
@@ -3,6 +3,8 @@
 class FlowSearchReflex < ApplicationReflex
   def search
     flows = Flow.where('name LIKE ?', "%#{params[:query]}%")
+    puts "Flows found: #{flows.count}" # Debugging
     morph '#flows-list', render(partial: 'flows/flows', locals: { flows: flows })
+    # morph '#flows-list', render(partial: 'flows/flows', locals: { flows: flows })
   end
 end
