ffffb3d057809aee314f5eeafd09f8efb92fdbca
grantfogle <grantjfogle@gmail.com>
2024-04-11 22:43:52 -0600

    added basic user state check in maps controller to redirect to sign in if not logged in

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,5 +1,11 @@
 class MapsController < ApplicationController
   def show
-    @flows = nil
+    if Current.user
+      @flows = nil
+    else
+    # check if user is logged in,
+    # if not redirect to login page
+      redirect_to sign_in_path, alert: 'You must be logged in to view this page.' unless Current.user
+    end
   end
 end
