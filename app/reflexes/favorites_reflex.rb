eab7a2ec5ff6ab70de229f4d6dc2dc86b0a67e4c
grantfogle <grantjfogle@gmail.com>
2024-04-29 21:50:25 -0600

    adding notes for favorites reflex

--- app/reflexes/favorites_reflex.rb
+++ app/reflexes/favorites_reflex.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class FavoritesReflex < ApplicationReflex
+    # use favorite service to do maths
+    def remove_favorite
+      flow = Flow.find(element.dataset[:flow_id])
+      favorite = Favorite.find_by(flow_id: flow.id, user_id: connection.current_user.id)
+  
+      if favorite
+        favorite.destroy
+        @message = 'Removed from favorites.'
+        # morph selector
+        # morph 'favo'
+      else
+        connection.current_user.favorites.create(flow: flow)
+        @message = 'Added to favorites.'
+      end
+    end
 end
