a53d3937cff67b522acd4510dd91c3df87c4866b
grantfogle <grantjfogle@gmail.com>
2024-05-04 14:01:00 -0600

    wip: trying to diagnose why my favorite reflex is not updating element

--- app/javascript/controllers/favorites_controller.js
+++ app/javascript/controllers/favorites_controller.js
@@ -1,31 +1,8 @@
 import ApplicationController from "./application_controller"
 
 export default class extends ApplicationController {
-  // connect () {
-  //   super.connect()
-  // }
-
   connect() {
     super.connect()
-    this.stimulate('FavoritesReflex#toggle_favorite').then(() => {
-      console.log('Reflex has finished')
-    })
-  }
-  
-  beforeReflex(element, reflex) {
-    console.log(`Before reflex: ${reflex}`)
-  }
-  
-  reflexSuccess(element, reflex, error) {
-    console.log(`Reflex success: ${reflex}`)
-  }
-  
-  reflexError(element, reflex, error) {
-    console.error(`Reflex error: ${error}`)
-  }
-  
-  afterReflex(element, reflex) {
-    console.log(`After reflex: ${reflex}`)
   }
   
 }
