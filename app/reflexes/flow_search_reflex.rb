625b583e3acbc18286df7ddceba4994a81003c51
grantfogle <grantjfogle@gmail.com>
2024-05-06 22:22:43 -0600

    wip: just going in circles at this point, i need to get a better plan of what i want to do

--- app/reflexes/flow_search_reflex.rb
+++ app/reflexes/flow_search_reflex.rb
@@ -3,10 +3,17 @@
 class FlowSearchReflex < ApplicationReflex
     def search
         @flows = Flow.where("name LIKE ?", "%#{params[:query]}%").limit(20)
+        # @flows = Flow.select("flows.*, EXISTS (
+        #                   SELECT 1
+        #                   FROM favorites
+        #                   WHERE favorites.flow_id = flows.id AND favorites.user_id = #{user_id}
+        #                 ) AS favorited")
+        #        .where("flows.name LIKE ?", "%#{params[:query]}%")
+        #        .limit(20)
         ## i have commented out the selector morph
         ## because i want to ensure that the logic for favorited/unfavoriting a flow
         ## still works on the page
-        # morph "#flows-table", render(partial: "flows/flows", locals: { flows: @flows })
+        morph "#flows-table", render(partial: "flows/flows", locals: { flows: @flows })
     end
 
     def map_search
