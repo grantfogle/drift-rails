177b491560caf26bff47c683805cb38ac3b91a4b
grantfogle <grantjfogle@gmail.com>
2025-02-27 19:03:51 -0700

    added popup on click to map

--- app/javascript/packs/maps/show.js
+++ app/javascript/packs/maps/show.js
@@ -12,10 +12,9 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
-
-
   // 2. add layers and sources
   map.on('load', function () {
+    // what other souces can i get?
     SOURCES.forEach((source) => {
       map.addSource(source.id, source.data);
     });
@@ -23,30 +22,65 @@ document.addEventListener('DOMContentLoaded', function() {
     LAYERS.forEach((layer) => {
       map.addLayer(layer);
 
+      // 3. toggle layers
       const checkbox = document.getElementById(`toggle-${layer.id}`);
 
       if (checkbox) {
         checkbox.addEventListener('change', (e) => toggleLayer(map, layer.id, e));
       }
+
+
+      // 4. click events on POI's
+      addMouseInteractions(map, layer.id);
+
+      // 5. show popups
+      addClickPopup(map, layer.id);
     });
   });
-
-
-  // click events on POI's
-  // show popups
-  // toggle layers
-
-  // ... existing code for map.on('load') and other event listeners ...
 });
 
 // used to toggle layers on map
 // layerId must match the id of the layer in _layers.html.erb partial
-function toggleLayer(map, layerId, e) {
+const toggleLayer = (map, layerId, e) => {
   var visibility = map.getLayoutProperty(layerId, 'visibility');
+  // set local storage on change so when the page reloads, the layer is still toggled
+  // make it so that the layer isn't loaded if it's not toggled,
+  // and if it is toggled, then it's loaded
 
   if (e.target.checked) {
     map.setLayoutProperty(layerId, 'visibility', 'visible');
   } else {
     map.setLayoutProperty(layerId, 'visibility', 'none');
   }
+}
+
+const addMouseInteractions = (map, layerId) => {
+  map.on('mouseenter', layerId, () => {
+    map.getCanvas().style.cursor = 'pointer';
+  });
+
+  map.on('mouseleave', layerId, () => {
+    map.getCanvas().style.cursor = '';
+  });
+}
+
+const addClickPopup = (map, layerId) => {
+  map.on('click', layerId, (e) => {
+    var features = map.queryRenderedFeatures(e.point, {
+      layers: [layerId]
+    });
+  
+    if (!features.length) {
+      return;
+    }
+  
+    var feature = features[0];
+  
+    // Perform actions with the feature, e.g., display a popup
+    // how to style popup?
+    var popup = new mapboxgl.Popup()
+      .setLngLat(feature.geometry.coordinates)
+      .setText('You clicked on ' + feature.properties.name) // Assuming 'name' is a property of the feature
+      .addTo(map);
+    });
 }
